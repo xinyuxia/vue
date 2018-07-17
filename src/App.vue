0b9b90ee3eceeb85b560c87c9d17138b23fe9963
gx <2544512956@qq.com>
2018-07-17 16:18:46 +0800

    commit 2018/7/17

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h2 class="color">博客</h2>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style src="../a.css">
+
+</style>
